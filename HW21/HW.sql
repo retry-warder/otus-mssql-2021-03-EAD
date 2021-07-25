474cd4a1fa9d2cac28ac4a2443f94f4877187a4b
retry-warder <60867460+retry-warder@users.noreply.github.com>
2021-07-25 11:50:54 +0700

    Update HW.sql

--- HW21/HW.sql
+++ HW21/HW.sql
@@ -1,17 +1,17 @@
-Select
+Select 
 ord.CustomerID, det.StockItemID, det.UnitPrice, det.Quantity, ord.OrderID
 INTO #ord
-FROM
+FROM 
 Sales.Orders AS ord
-INNER HASH JOIN Sales.OrderLines AS det ON det.OrderID = ord.OrderID
+INNER HASH JOIN Sales.OrderLines AS det ON det.OrderID = ord.OrderID 
 INNER HASH JOIN Warehouse.StockItems AS SI ON SI.StockItemID = det.StockItemID AND SI.SupplierId = 12
-INNER HASH JOIN Sales.Invoices AS Inv ON Inv.OrderID = ord.OrderID
+INNER HASH JOIN Sales.Invoices AS Inv  ON Inv.OrderID = ord.OrderID
 INNER HASH JOIN Sales.CustomerTransactions AS Trans ON Trans.InvoiceID = Inv.InvoiceID
 WHERE
-Inv.InvoiceDate = ord.OrderDate --DATEDIFF(dd, Inv.InvoiceDate, ord.OrderDate) убрал т.к. все равно тип данных date, так, что выражение не имеет смысла
-AND Inv.BillToCustomerID != ord.CustomerID
-AND (SELECT SUM(Total.UnitPrice*Total.Quantity) FROM Sales.OrderLines AS Total INNER HASH JOIN Sales.Orders AS ordTotal On ordTotal.OrderID = Total.OrderID WHERE ordTotal.CustomerID = Inv.CustomerID) > 250000
-CREATE NONCLUSTERED INDEX IDX_#ord ON [dbo].[#ord] (CustomerID)
+Inv.BillToCustomerID != ord.CustomerID 
+AND Inv.InvoiceDate =  ord.OrderDate --DATEDIFF(dd, Inv.InvoiceDate, ord.OrderDate) убрал т.к. все равно тип данных date, так, что выражение не имеет смысла
+AND (SELECT SUM(Total.UnitPrice*Total.Quantity) FROM Sales.OrderLines AS Total INNER HASH JOIN Sales.Orders AS ordTotal On ordTotal.OrderID = Total.OrderID WHERE ordTotal.CustomerID = Inv.CustomerID) > 250000 
+CREATE NONCLUSTERED INDEX IDX_#ord ON [dbo].[#ord] (StockItemID)
 
 SELECT T.CustomerID, T.StockItemID, SUM(T.UnitPrice), SUM(T.Quantity), COUNT(T.OrderID) FROM #ord AS T
 INNER JOIN Warehouse.StockItemTransactions AS ItemTrans ON ItemTrans.StockItemID = T.StockItemID
