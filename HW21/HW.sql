6306eba533733ea992c2d5875cef1945b809420d
retry-warder <60867460+retry-warder@users.noreply.github.com>
2021-07-25 14:23:04 +0700

    Update HW.sql

--- HW21/HW.sql
+++ HW21/HW.sql
@@ -1,41 +1,45 @@
-Select 
-ord.CustomerID, det.StockItemID, det.UnitPrice, det.Quantity, ord.OrderID
-INTO #ord
-FROM 
-Sales.Orders AS ord
-INNER HASH JOIN Sales.OrderLines AS det ON det.OrderID = ord.OrderID 
-INNER HASH JOIN Warehouse.StockItems AS SI ON SI.StockItemID = det.StockItemID AND SI.SupplierId = 12
-INNER HASH JOIN Sales.Invoices AS Inv  ON Inv.OrderID = ord.OrderID
-INNER HASH JOIN Sales.CustomerTransactions AS Trans ON Trans.InvoiceID = Inv.InvoiceID
-WHERE
-Inv.BillToCustomerID != ord.CustomerID 
-AND Inv.InvoiceDate =  ord.OrderDate --DATEDIFF(dd, Inv.InvoiceDate, ord.OrderDate) убрал т.к. все равно тип данных date, так, что выражение не имеет смысла
-AND (SELECT SUM(Total.UnitPrice*Total.Quantity) FROM Sales.OrderLines AS Total INNER HASH JOIN Sales.Orders AS ordTotal On ordTotal.OrderID = Total.OrderID WHERE ordTotal.CustomerID = Inv.CustomerID) > 250000 
-CREATE NONCLUSTERED INDEX IDX_#ord ON [dbo].[#ord] (StockItemID)
+SELECT ordTotal.CustomerID 
+	INTO #CUSTOMERS
+	FROM Sales.Orders  AS ordTotal 
+	INNER HASH JOIN Sales.OrderLines AS Total 
+	On ordTotal.OrderID = Total.OrderID 
+	GROUP BY ordTotal.CustomerID
+	HAVING SUM(Total.UnitPrice*Total.Quantity)  > 250000
+CREATE NONCLUSTERED INDEX IDX_#CUSTOMERS ON #CUSTOMERS (CustomerID)
 
-SELECT T.CustomerID, T.StockItemID, SUM(T.UnitPrice), SUM(T.Quantity), COUNT(T.OrderID) FROM #ord AS T
-INNER JOIN Warehouse.StockItemTransactions AS ItemTrans ON ItemTrans.StockItemID = T.StockItemID
-GROUP BY T.CustomerID, T.StockItemID ORDER BY T.CustomerID, T.StockItemID
+SELECT 
+	N.StockItemID
+	INTO #PHONE_COMPANY_StockItem
+	FROM [WideWorldImporters].[Warehouse].[StockItemTransactions] AS N
+	INNER JOIN Warehouse.StockItems AS T
+	ON N.StockItemID = T.StockItemID
+	AND T.SupplierId = 12;
+CREATE NONCLUSTERED INDEX IDX_#PHONE_COMPANY_StockItem ON #PHONE_COMPANY_StockItem (StockItemID)
 
-DROP TABLE #ord
 
---ИЛИ
+SELECT I.OrderID, I.BillToCustomerID, I.InvoiceDate 
+INTO #Invoices
+FROM   Sales.Invoices AS I
+INNER JOIN #CUSTOMERS AS C ON I.CustomerID = C.CustomerID 
+INNER JOIN Sales.CustomerTransactions AS Trans ON Trans.InvoiceID = I.InvoiceID 
+OPTION (MAXDOP 1)
+CREATE NONCLUSTERED INDEX IDX_#Invoices ON #Invoices (OrderID, InvoiceDate)
+
 
 Select 
 ord.CustomerID, det.StockItemID, SUM(det.UnitPrice), SUM(det.Quantity), COUNT(ord.OrderID)
 FROM 
-Sales.Orders AS ord
+#CUSTOMERS AS C
+INNER HASH JOIN Sales.Orders AS ord
+ON ord.CustomerID = C.CustomerID 
+INNER HASH JOIN #Invoices AS Inv ON Inv.OrderID = ord.OrderID AND Inv.InvoiceDate =  ord.OrderDate
 INNER HASH JOIN Sales.OrderLines AS det ON det.OrderID = ord.OrderID 
-INNER JOIN Warehouse.StockItems AS SI ON SI.SupplierId = 12 AND SI.StockItemID = det.StockItemID
-INNER HASH JOIN Sales.Invoices AS Inv  
-ON Inv.OrderID = ord.OrderID
-INNER HASH JOIN Sales.CustomerTransactions AS Trans ON Trans.InvoiceID = Inv.InvoiceID
-INNER HASH JOIN Warehouse.StockItemTransactions AS ItemTrans ON ItemTrans.StockItemID = det.StockItemID
+INNER HASH JOIN #PHONE_COMPANY_StockItem AS ItemTrans ON ItemTrans.StockItemID = det.StockItemID
 WHERE
 Inv.BillToCustomerID != ord.CustomerID
-AND
-Inv.InvoiceDate =  ord.OrderDate  --DATEDIFF(dd, Inv.InvoiceDate, ord.OrderDate) убрал т.к. все равно тип данных date, так, что выражение не имеет смысла
-AND
-(SELECT SUM(Total.UnitPrice*Total.Quantity) FROM Sales.OrderLines AS Total INNER HASH JOIN Sales.Orders AS ordTotal On ordTotal.OrderID = Total.OrderID WHERE ordTotal.CustomerID = Inv.CustomerID) > 250000
 GROUP BY ord.CustomerID, det.StockItemID ORDER BY ord.CustomerID, det.StockItemID
 OPTION (MAXDOP 1)
+
+DROP TABLE #CUSTOMERS
+DROP TABLE #PHONE_COMPANY_StockItem
+DROP TABLE #Invoices
